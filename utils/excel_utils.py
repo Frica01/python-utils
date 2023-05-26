68a734e894c662486bb12d975e124dba38a647c7
frica01 <819682099@qq.com>
2023-05-26 17:46:34 +0800

    add excel_utils,window_utils

---
+++ utils/excel_utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Name:         excel_utils.py
+# Author:       小菜
+# Date:         2023/5/26 15:41
+# Description:
+
+"""
+安装pandas、dict_to_excel
+"""
+
+import pandas as pd
+
+
+def dict_to_excel(data_dict: dict, file_path: str) -> None:
+    """
+    将字典数据添加到Excel文件中。
+
+    Args：
+        data_dict (dict): 包含要添加到Excel文件的数据的字典。
+                          字典的键（key）为列名称，值（value）为对应的内容。
+                          默认格式为dict(str:list()...)
+        file_path (str): Excel文件的路径。
+
+    Returns：
+        None
+    """
+
+    # 将字典数据转换为DataFrame
+    df = pd.DataFrame.from_dict(data_dict, orient='index').transpose()
+
+    # 尝试读取现有的Excel文件，如果文件不存在则创建新文件
+    try:
+        existing_data = pd.read_excel(file_path)
+        df = pd.concat([existing_data, df], ignore_index=True)
+    except FileNotFoundError:
+        pass
+
+    # 将DataFrame写入Excel文件
+    df.to_excel(file_path, index=False)
--- utils/window_utils.py
+++ utils/window_utils.py
@@ -3,3 +3,124 @@
 # Author:       小菜
 # Date:         2023/4/28 14:23
 # Description:
+
+import win32gui
+import win32con
+
+
+# 窗口定位
+
+
+def get_window_handle(class_name=None, title=None):
+    """
+    通过类名和标题查找窗口句柄.
+
+    Args:
+        class_name(str|None):窗口的类名. 默认为None.
+        title(str|None):窗口的标题. 默认为None.
+
+    Returns:
+        int: 返回找到的窗口句柄，如果没有找到则返回0.
+    """
+    return win32gui.FindWindow(class_name, title)
+
+
+# 窗口最小化
+def minimize_window(class_name=None, title=None):
+    """
+    窗口最小化
+
+    Args:
+        class_name(str|None):窗口的类名. 默认为None.
+        title(str|None):窗口的标题. 默认为None.
+
+    Returns:
+        None
+    """
+    hwnd = get_window_handle(class_name=class_name, title=title)
+    win32gui.ShowWindow(hwnd, win32con.SW_MINIMIZE)
+
+
+# 窗口最大化
+def maximize_window(class_name=None, title=None):
+    """
+    窗口最大化
+
+    Args:
+        class_name(str|None):窗口的类名. 默认为None.
+        title(str|None):窗口的标题. 默认为None.
+
+    Returns:
+        None
+    """
+    hwnd = get_window_handle(class_name=class_name, title=title)
+    win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
+
+
+# 窗口置顶
+def set_top_window(class_name=None, title=None):
+    """
+    窗口置顶
+
+    Args:
+        class_name(str|None):窗口的类名. 默认为None.
+        title(str|None):窗口的标题. 默认为None.
+
+    Returns:
+        None
+    """
+    hwnd = get_window_handle(class_name=class_name, title=title)
+    win32gui.SetForegroundWindow(hwnd)
+
+
+# 关闭窗口
+def close_window(class_name=None, title=None):
+    """
+    关闭窗口
+
+    Args:
+        class_name(str|None):窗口的类名. 默认为None.
+        title(str|None):窗口的标题. 默认为None.
+
+    Returns:
+        None
+    """
+    hwnd = get_window_handle(class_name=class_name, title=title)
+    win32gui.CloseWindow(hwnd)
+
+
+# 隐藏窗口
+def hide_window(class_name=None, title=None):
+    """
+    隐藏窗口
+
+    Args:
+        class_name(str|None):窗口的类名. 默认为None.
+        title(str|None):窗口的标题. 默认为None.
+
+    Returns:
+        None
+    """
+    hwnd = get_window_handle(class_name=class_name, title=title)
+    win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
+
+
+# 显示窗口
+def show_window(class_name=None, title=None):
+    """
+    显示窗口
+
+    Args:
+        class_name(str|None):窗口的类名. 默认为None.
+        title(str|None):窗口的标题. 默认为None.
+
+    Returns:
+        None
+    """
+    hwnd = get_window_handle(class_name=class_name, title=title)
+    win32gui.ShowWindow(hwnd, win32con.SW_SHOW)
+
+
+if __name__ == '__main__':
+    show_window('WeChatMainWndForPC', '微信')
+    maximize_window('WeChatMainWndForPC', '微信')
